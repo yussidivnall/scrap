11320e7b36192cd44e76fbce58c8cb1ba3ef9481
Jim Bob <Jim@Bob.car>
2023-06-13 16:04:09 +0200

    Youtube scraping progress

--- utils/scrape_youtube.py
+++ utils/scrape_youtube.py
@@ -7,21 +7,13 @@ from jsonpath_ng.ext import parser as json_parser
 from car_scraper import Json
 from car_scraper.Json.Helpers import del_path
 
-
-
-
-def find_path(entry, key, expression):
-
-    found = json_parser.parse(expression).find(entry)
-    # found = jsonpath_rw_ext.parse(expression).find(entry)
-    for f in found:
-        print(f.full_path)
-        print(json.dumps(f.value, indent=2))
+def dump(entry):
+    print(json.dumps(entry, indent=2))
     return entry
 
 def build_comments(entry):
-    found = json_parser.parse('$..commentThreadRenderer.comment.commentRenderer').find(entry)
-    entry["comments"]=[]
+    comments = []
+    found = json_parser.parse('$..commentRenderer').find(entry)
     for f in found:
         comment = {
             'author': f.value["authorText"]["simpleText"],
@@ -31,11 +23,11 @@ def build_comments(entry):
             'comment_id': f.value["commentId"],
             'likes': f.value["actionButtons"]["commentActionButtonsRenderer"]["likeButton"]["toggleButtonRenderer"]["accessibilityData"]["accessibilityData"]["label"],
             'video_id':f.value["publishedTimeText"]["runs"][0]["navigationEndpoint"]["watchEndpoint"]["videoId"],
-            # 'full_entry': f.value
         }
-        # print(json.dumps(comment, indent=2))
-        entry["comments"].append(comment)
-    return entry
+        comments.append(comment)
+    return { 'comments': comments }
+    # entry["comments"]=comments
+    # return entry
 
 
 def reject_unrelated_entries(entry):
@@ -62,40 +54,16 @@ def remove_unused_keys(entry, unused_keys):
     # print(json.dumps(entry,indent=2))
     return entry
 
-def get_comment_har_entries(har):
+def get_comments(har):
     template = {
-        'url': '$.request.url',
-        'content':'$.response.content.text'
+        'comments':'$.comments',
         }
     har_entries_path= "log.entries.item"
     preprocess_list = [
         { 'func': reject_unrelated_entries },
         { 'func': unescape_content },
-        # { 'func': find_path,
-        #   'args': {
-        #       'key': 'text',
-        #       # 'expression': '$.response.content.text.onResponseReceivedEndpoints.*.text[?(@.text =~ ".*Report.*")]'
-        #       'expression': '$..commentThreadRenderer.comment.commentRenderer'
-        #       # 'expression': '$.response.content.text.onResponseReceivedEndpoints..commentThreadRenderer.comment.commentRenderer'
-        #       # 'expression': '$.response.content.text.onResponseReceivedEndpoints..commentThreadRenderer.comment.commentRenderer..contentText..text'
-        #       # 'expression': '$.response.content.text.onResponseReceivedEndpoints..contentText..text'
-        #       # 'expression': '$..text'
-        #   }
-        # },
-        # {
-        #     'func': remove_unused_keys,
-        #     'args': {
-        #         'unused_keys': [
-        #             '$.request',
-        #             '$.pageref',
-        #             '$.startDateTime',
-        #             '$.response.headers',
-        #             '$.response.cookies',
-        #             '$..emojiIds',
-        #         ]
-        #     }
-        # },
         { 'func': build_comments },
+        # { 'func': dump },
     ]
 
     # postprocess_template = {
@@ -106,41 +74,18 @@ def get_comment_har_entries(har):
         template=template,
         path=har_entries_path,
         preprocess_list=preprocess_list,
-        # postprocess_template=postprocess_template,
     )
     return stream
-    #for entry in stream:
-    #    yield entry
-    #    #print(entry)
-
-def get_comments(content):
-    template = {
-        'text': '$.*.contentText.runs'
-    }
-
-    stream = Json.Parser.load_stream(
-        content,
-        template=template
-    )
-    # for e in stream:
-    #     print(e['text'])
-
-
-def scrape_comments(har):
-    stream = get_comment_har_entries(har)
-    for entry in stream:
-        # print(entry['url'])
-        # print(json.dumps(entry['content'], indent=2))
-        pass
-
-def scrape_comment_replies(har):
-    pass
-
 
 def scrape(har_files):
+    ret = []
     for har_file in har_files:
         with open(har_file, "rb") as fp:
-            scrape_comments(fp)
+            # for comments in scrape_comments(fp):
+            for comments in get_comments(fp):
+                ret.extend(comments["comments"])
+    print(json.dumps(ret, indent=2))
+    return ret
 
 if __name__=='__main__':
     arg_parser = argparse.ArgumentParser(description="Parse a YouTube har capture")
